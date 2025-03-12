0a6167b6bc9a8df5b57e51ba064ddb4da9e82559
leandromonaco <leandro.monaco@gmail.com>
2025-03-13 09:10:14 +1000

    Google Calendar API clean up

--- src/GoogleCalendar/GoogleCalendarConnector.cs
+++ src/GoogleCalendar/GoogleCalendarConnector.cs
@@ -15,92 +15,11 @@ namespace DevEx.Integrations.GoogleCalendar
             _api = RestService.For<IGoogleCalendar>("https://www.googleapis.com", settings);
         }
 
-        public async Task<List<GoogleCalendarEvent>> GetPublicHolidaysAsync(CountryEnum country)
+        public async Task<List<GoogleCalendarEvent>> GetPublicHolidaysAsync(string calendarId)
         {
-            var result = await _api.FetchCountryHolidays(GetCountryCode(country), _apiKey);
+            var result = await _api.GetPublicHolidays(calendarId, _apiKey);
             var calendar = result.items;
             return calendar;
         }
-
-        public async Task<List<GoogleCalendarEvent>> GetCalendarEventsAsync(string calendarId)
-        {
-            var result = await _api.FetchEvents(calendarId, _apiKey);
-            var calendar = result.items;
-            return calendar;
-        }
-
-        private string GetCountryCode(CountryEnum country)
-        {
-            switch (country)
-            {
-                case CountryEnum.Australia:
-                    return "en.australian";
-                case CountryEnum.Pakistan:
-                    return "en.pk";
-                case CountryEnum.Philippines:
-                    return "en.philippines";
-                case CountryEnum.Vietnam:
-                    return "en.vietnamese";
-                case CountryEnum.India:
-                    return "en.indian";
-                default:
-                    return string.Empty;
-            }
-        }
-
-
-        /*
-            Here is updated on January 2020 list ISO 3166-2 to google calendar name:
-            IMPORTANT! For all not listed countries ISO 3166-2 is used so i.e. for Albania it is al.
-
-        {"au", "australian"},
-        {"at", "austrian"},
-        {"br", "brazilian"},
-        {"bg", "bulgarian"},
-        {"ca", "canadian"},
-        {"cn", "china"},
-        {"hr", "croatian"},
-        {"cz", "czech"},
-        {"dk", "danish"},
-        {"fi", "finnish"},
-        {"fr", "french"},
-        {"de", "german"},
-        {"gr", "greek"},
-        {"hk", "hong_kong"},
-        {"hu", "hungarian"},
-        {"in", "indian"},
-        {"id", "indonesian"},
-        {"ie", "irish"},
-        {"il", "jewish"},
-        {"it", "italian"},
-        {"jp", "japanese"},
-        {"lv", "latvian"},
-        {"lt", "lithuanian"},
-        {"my", "malaysia"},
-        {"mx", "mexican"},
-        {"nl", "dutch"},
-        {"nz", "new_zealand"},
-        {"no", "norwegian"},
-        {"ph", "philippines"},
-        {"pl", "polish"},
-        {"pt", "portuguese"},
-        {"ro", "romanian"},
-        {"ru", "russian"},
-        {"sa", "saudiarabian"},
-        {"sg", "singapore"},
-        {"sk", "slovak"},
-        {"si", "slovenian"},
-        {"kr", "south_korea"},
-        {"es", "spain"},
-        {"se", "swedish"},
-        {"tw", "taiwan"},
-        {"tl", "thai"},
-        {"tr", "turkish"},
-        {"ua", "ukrainian"},
-        {"us", "usa"},
-        {"vn", "vietnamese"}
-
-         
-         */
     }
 }
--- src/IGoogleCalendar.cs
+++ src/IGoogleCalendar.cs
@@ -8,11 +8,7 @@ namespace DevEx.Integrations
     /// </summary>
     internal interface IGoogleCalendar
     {
-        [Get("/calendar/v3/calendars/{countryCode}%23holiday%40group.v.calendar.google.com/events?key={apiKey}")]
-        Task<GoogleCalendarResponse> FetchCountryHolidays([AliasAs("countryCode")] string countryCode, string apiKey);
-
-        [Get("/calendar/v3/calendars/{calendarId}%40group.calendar.google.com/events?key={apiKey}")]
-        Task<GoogleCalendarResponse> FetchEvents([AliasAs("calendarId")] string calendarId, string apiKey);
-
+        [Get("/calendar/v3/calendars/{calendarId}.official%23holiday%40group.v.calendar.google.com/events?key={apiKey}")]
+        Task<GoogleCalendarResponse> GetPublicHolidays([AliasAs("calendarId")] string calendarId, string apiKey);
     }
 }
